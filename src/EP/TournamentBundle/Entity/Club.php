c895d9252f780f02be2a0a13309d1c1519784ced
eprevot <eprevot@patschool.com>
2015-04-09 15:57:16 +0200

    first entity generated

---
+++ src/EP/TournamentBundle/Entity/Club.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace EP\TournamentBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Club
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="EP\TournamentBundle\Entity\ClubRepository")
+ */
+class Club
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="address", type="string", length=255)
+     */
+    private $address;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="city", type="string", length=255)
+     */
+    private $city;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="fft_id", type="string", length=255)
+     */
+    private $fftId;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Club
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set address
+     *
+     * @param string $address
+     * @return Club
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    /**
+     * Get address
+     *
+     * @return string 
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * Set city
+     *
+     * @param string $city
+     * @return Club
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    /**
+     * Get city
+     *
+     * @return string 
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * Set fftId
+     *
+     * @param string $fftId
+     * @return Club
+     */
+    public function setFftId($fftId)
+    {
+        $this->fftId = $fftId;
+
+        return $this;
+    }
+
+    /**
+     * Get fftId
+     *
+     * @return string 
+     */
+    public function getFftId()
+    {
+        return $this->fftId;
+    }
+}
