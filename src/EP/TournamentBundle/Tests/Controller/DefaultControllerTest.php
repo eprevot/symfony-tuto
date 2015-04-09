e7876affae7e0309abbb02cdd86b4d1122968f90
eprevot <eprevot@patschool.com>
2015-04-09 16:35:52 +0200

    update first test

--- src/EP/TournamentBundle/Tests/Controller/DefaultControllerTest.php
+++ src/EP/TournamentBundle/Tests/Controller/DefaultControllerTest.php
@@ -10,8 +10,9 @@ class DefaultControllerTest extends WebTestCase
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/hello/Fabien');
+        $crawler = $client->request('GET', '/tournament/Paris');
 
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        $this->assertTrue($crawler->filter('html:contains("Tournaments")')->count() > 0);
+        $this->assertTrue($crawler->filter('html:contains("Paris")')->count() > 0);
     }
 }
