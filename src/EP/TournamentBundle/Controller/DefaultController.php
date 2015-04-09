05e0130e280c90afe3b42cf9d8750caca91a4eae
eprevot <eprevot@patschool.com>
2015-04-09 11:31:22 +0200

    add parameters to bundle and display in view through controller

--- src/EP/TournamentBundle/Controller/DefaultController.php
+++ src/EP/TournamentBundle/Controller/DefaultController.php
@@ -8,6 +8,8 @@ class DefaultController extends Controller
 {
     public function indexAction($name)
     {
-        return $this->render('EPTournamentBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('EPTournamentBundle:Default:index.html.twig', array('name' => $name,
+                                                                                 'default_cost' => $this->container->getParameter('default_cost'),
+                                                                                 'default_surface' => $this->container->getParameter('default_surface')));
     }
 }
